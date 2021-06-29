74407195aca504500afd392399feef5e00c9ea53
Fiodar <mecitan@gmail.com>
2021-06-29 14:11:03 +0300

    init: начал выполнение задачи список дел

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+    <form>
+        <input type="text" v-model="value" placeholder="Введите текст">
+        <input type="button" v-on:click.prevent="currentValue" value="Click">
+        <p>Сообщение: {{ value }}</p>
+    </form>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        currentValue: function(event) {
+            if(this.value === this.value) {
+                event.target.value
+            }
+        }
+    }
+}
+</script>
