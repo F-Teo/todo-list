0298790e5ec2ac46922f09176214e4ec47e9735b
Fiodar <mecitan@gmail.com>
2021-06-30 19:58:24 +0300

    create: создал список, который выводит данные из массива

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ol>
+        <li v-for="item in listValue" v-bind:key="item.name">
+            {{ item.name }}
+        </li>
+    </ol>
+</template>
+
+<script>
+export default {
+    props: {
+        listValue: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
